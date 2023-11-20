0fc36caae2d4013c4a042bf738f49db773ad13e4
Johnny Cheng <itainan.101@gmail.com>
2023-11-21 00:30:46 +0900

    implent basic lexer
    https://youtu.be/8VB5TY1sIRo
    episode 1
    
    Signed-off-by: Johnny Cheng <itainan.101@gmail.com>

--- lexer.py
+++ lexer.py
@@ -1,216 +1,97 @@
 import re
-from enum import Enum, auto
 from dataclasses import dataclass
+from enum import Enum
+from enum import auto
 
 
 class TokenType(Enum):
-    Name = auto()
-    Indent = auto()
+    # Literal Types
+    Identifier = auto()
     Number = auto()
-    Operator = auto()
-    String = auto()
-    NewLine = auto()
-    EndMarker = auto()
+
+    # keywords
+    Let = auto()
+
+    # Grouping * Operators
+    BinaryOp = auto()
+    Equals = auto()
+    OpenParen = auto()
+    CloseParen = auto()
 
     def __repr__(self) -> str:
         return self.name
 
+    @staticmethod
+    def find_special_type(keyword: str):
+        maps = {
+            "令": TokenType.Let,
+            "為": TokenType.Equals,
+        }
+        return maps.get(keyword, None)
+
 
 @dataclass
-class TokenInfo:
+class Token:
     type: TokenType
-    string: str
-    start: tuple[int, int]
-    end: tuple[int, int]
-    line: str
+    value: str
+    ## TODO
+    # start: tuple[int, int]
+    # end: tuple[int, int]
+    # line: str
 
 
-def _startswith_keyword(txt: str) -> str:
-    for keyword in {
-        "為每個",  # for
-        "存在於",  # in
-        "或若",  # elif
-        "不然",  # else
-        "每當",  # while
-        "引用",  # import
-        "令",  # let
-        "為",  # assignment
-        "是",  # true
-        "否",  # false
-        "若",  # if
-        "或",  # else
-    }:
-        if txt.startswith(keyword):
-            return keyword
-    return ""
+def _is_skippable(char: str) -> bool:
+    return char in " \n\r\t"
 
 
-def _startswith_operator(txt: str) -> bool:
-    for op in {
-        "：",  # :
-        "（",  # (
-        "）",  # )
-        "《",  # [
-        "》",  # ]
-        "，",  # dot notation
-        "。",  # ; notation
-        "等於",
-        "不等於",
-        "大於",
-        "大於等於",
-        "小於",
-        "小於等於",
-    }:
-        if txt.startswith(op):
-            return True
-    return False
+def tokenize(src_code: str) -> list[Token]:
+    src = src_code
 
-
-def _startswith_spaces(txt: str) -> str:
-    if m := re.match(r"^\s+", txt):
-        return m.group(0)
-    return ""
-
-
-def _is_number_string(txt: str) -> bool:
-    try:
-        # TODO: use a safer eval function
-        eval(txt)
-        return True
-    except:
-        return False
-
-
-class State(Enum):
-    CheckIndent = auto()
-    GetChar = auto()
-    GetString = auto()
-    AppendToken = auto()
-    ClearBuffer = auto()
-
-
-def _tokenize_line(lineno: int, line: str):
     tokens = []
-    col = 0
-    txt = ""
+    while src != "":
+        # take care of fullwidth characters too
+        match src[0]:
+            case "(" | "（":
+                tokens.append(Token(TokenType.OpenParen, src[0]))
+                src = src[1:]
+            case ")" | "）":
+                tokens.append(Token(TokenType.CloseParen, src[0]))
+                src = src[1:]
+            case "+" | "-" | "*" | "/" | "＋" | "－" | "×" | "÷":
+                tokens.append(Token(TokenType.BinaryOp, src[0]))
+                src = src[1:]
+            case "=" | "＝":
+                tokens.append(Token(TokenType.Equals, src[0]))
+                src = src[1:]
+            case _:
+                if src[0].isnumeric():
+                    char_cnt = 0
+                    src_len = len(src)
+                    while char_cnt < src_len and src[char_cnt].isnumeric():
+                        char_cnt += 1
+                    num, src = src[:char_cnt], src[char_cnt:]
+                    tokens.append(Token(TokenType.Number, num))
+                elif src[0].isalpha():
+                    char_cnt = 0
+                    src_len = len(src)
+                    while char_cnt < src_len and src[char_cnt].isalpha():
+                        char_cnt += 1
+                    alpha, src = src[:char_cnt], src[char_cnt:]
 
-    in_string = False
-    state = State.CheckIndent
-    while True:
-        match state:
-            case State.CheckIndent:
-                if indent := _startswith_spaces(line):
-                    tokens.append(
-                        TokenInfo(
-                            TokenType.Indent,
-                            indent,
-                            (lineno, col),
-                            (lineno, col + len(indent)),
-                            line,
-                        )
-                    )
-                    col += len(indent)
-                state = State.GetChar
-            case State.GetChar:
-                if col >= len(line):
-                    break
-                txt += line[col]
-                col += 1
-                state = State.AppendToken
-            case State.AppendToken:
-                if in_string:
-                    if txt.endswith("」"):
-                        tokens.append(
-                            TokenInfo(
-                                TokenType.String,
-                                txt,
-                                (lineno, col - len(txt)),
-                                (lineno, col),
-                                line,
-                            )
-                        )
-                        in_string = False
-                        state = State.ClearBuffer
-                    else:
-                        state = State.GetChar
-                elif txt.startswith("「"):
-                    in_string = True
-                    state = State.GetChar
-                elif longest := _startswith_keyword(line[col - len(txt) :]):
-                    col += len(longest) - len(txt)
-                    tokens.append(
-                        TokenInfo(
-                            TokenType.Name,
-                            longest,
-                            (lineno, col - len(longest)),
-                            (lineno, col),
-                            line,
-                        )
-                    )
-                    state = State.ClearBuffer
-                elif _startswith_operator(txt):
-                    tokens.append(
-                        TokenInfo(
-                            TokenType.Operator,
-                            txt,
-                            (lineno, col - len(txt)),
-                            (lineno, col),
-                            line,
-                        )
-                    )
-                    state = State.ClearBuffer
-                elif txt == "\n":
-                    tokens.append(
-                        TokenInfo(
-                            TokenType.NewLine,
-                            txt,
-                            (lineno, col - len(txt)),
-                            (lineno, col),
-                            line,
-                        )
-                    )
-                    state = State.ClearBuffer
-                elif (
-                    col >= len(line) - 1
-                    or _startswith_keyword(line[col:])
-                    or _startswith_operator(line[col:])
-                ):
-                    if _is_number_string(txt):
-                        tokens.append(
-                            TokenInfo(
-                                TokenType.Number,
-                                txt,
-                                (lineno, col - len(txt)),
-                                (lineno, col),
-                                line,
-                            )
-                        )
-                    else:
-                        clean_txt = txt.rstrip()
-                        tokens.append(
-                            TokenInfo(
-                                TokenType.Name,
-                                clean_txt,
-                                (lineno, col - len(txt)),
-                                (lineno, col - len(txt) + len(clean_txt)),
-                                line,
-                            )
-                        )
-                    state = State.ClearBuffer
-                elif _startswith_spaces(txt):
-                    state = State.ClearBuffer
+                    t = TokenType.find_special_type(alpha) or TokenType.Identifier
+                    tokens.append(Token(t, alpha))
+                elif _is_skippable(src[0]):
+                    src = src[1:]
                 else:
-                    state = State.GetChar
-            case State.ClearBuffer:
-                txt = ""
-                state = State.GetChar
+                    raise NotImplementedError(f"Unknown token: {src[0]}")
+
     return tokens
 
-
 if __name__ == "__main__":
-    tokens = []
-    with open("chpy.ch") as fs:
-        for line_no, line in enumerate(fs.readlines()):
-            tokens += _tokenize_line(line_no, line)
+    from pprint import pprint
+    tokens = tokenize("令 x = 1 + 2")
+    pprint(tokens)
 
-    print(tokens)
+    tokens = tokenize("令 數字 為 1 + 2")
+    pprint(tokens)
+
